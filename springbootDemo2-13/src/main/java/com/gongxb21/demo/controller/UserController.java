617078286203cd05e25db6544c3d42c1e90c1a9f
gongxb21 <gongxb2260@outlook.com>
2018-01-07 13:41:54 +0800

    download word

--- springbootDemo2-13/src/main/java/com/gongxb21/demo/controller/UserController.java
+++ springbootDemo2-13/src/main/java/com/gongxb21/demo/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.gongxb21.demo.controller;
 
+import java.io.File;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import com.gongxb21.demo.jpa.bean.User;
 import com.gongxb21.demo.jpa.repository.UserRepository;
@@ -21,6 +27,8 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+
+import freemarker.template.Template;
 
 /**
  * @author gongxb
@@ -33,7 +41,9 @@ public class UserController {
 	private static final Logger logger =LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
-	
+	@Autowired
+	private FreeMarkerConfigurer freeMarkerConfigurer;
+	private Base64 b64Encoder=new Base64();
 	@RequestMapping("/downloadpdf")
 	public void download(HttpServletRequest request,HttpServletResponse reponse) throws Exception {
 		logger.info("down load method");
@@ -90,5 +100,22 @@ public class UserController {
 		String fileName="user.xls";
 		ExportExcelUtil.exportExcel(request, response, fileName, data);
 		
+	}
+	@RequestMapping("/downloadword")
+	public void downloadWord(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		logger.info(" download word method");
+		List<User> list=userRepository.findByIdLessThan(30);
+		response.setHeader("content-type", "application/msword");
+		response.setHeader("Content-Disposition", "attachment;filename=userList.doc");
+		freeMarkerConfigurer.getConfiguration().setClassForTemplateLoading(getClass(), "/");
+		Template template=freeMarkerConfigurer.getConfiguration().getTemplate("userList.ftl");
+		String webapp=request.getServletContext().getRealPath("/");
+//		for(int i=0;i<list.size();i++) {
+//			User user=list.get(i);
+//			File file=new File(webapp,user.get);
+//		}
+		Map<String,Object> root =new HashMap<String,Object>();
+		root.put("list", list);
+		template.process(root, new OutputStreamWriter(response.getOutputStream()));
 	}
 }
